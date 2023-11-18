b039c4882f652bb0d9fd269cf39b7e784ff47974
blfuentes <blfuentes@mundo-r.com>
2023-11-18 20:06:46 +0100

    finished hc-sr501-test

--- hc-sr501-test/src/main.cpp
+++ hc-sr501-test/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int led = 9;    // Led positive terminal to the digital pin 9.
+const int sensor = 5; // signal pin of sensor to digital pin 5.
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int prevState;
+int currentState;
+int sensorVal;
+
+void setup()
+{                         // Void setup is ran only once after each powerup or reset of the Arduino board.
+  pinMode(led, OUTPUT);   // Led is determined as an output here.
+  pinMode(sensor, INPUT); // PIR motion sensor is determined is an input here.
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{ // Void loop is ran over and over and consists of the main program.
+  sensorVal = digitalRead(sensor);
+  if (sensorVal == HIGH)
+  {
+    digitalWrite(led, sensorVal);
+    delay(500); // Delay of led is 500
+    if (currentState == LOW)
+    {
+      Serial.println("Motion detected");
+      currentState = HIGH;
+    }
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+    delay(500);
+    if (currentState == HIGH)
+    {
+      Serial.println("The action/motion has stopped");
+      currentState = LOW;
+    }
+  }
 }
